b40c0cd3b8cd92d7b84dc22a59c2c811de71532c
Naresh Tak <taknaresh515@gmail.com>
2023-02-01 12:25:48 +0530

    bharat pay

---
+++ slider.js
@@ -0,0 +1,17 @@
+
+$('.slider_container').slick({
+  indicators: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  pauseOnHover: false,
+});
+$('.customer-logos').slick({
+  slidesToShow: 5,
+  autoplay: true,
+  autoplaySpeed: 500,
+  variableWidth: true,
+  pauseOnHover: false,
+});
